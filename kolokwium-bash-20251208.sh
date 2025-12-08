5bea332f09a1c86bdc6f692cf2b74578ffbd6339
Roksana <laubaroksana@gmail.com>
2025-12-08 14:35:27 +0100

    kolokwium-bash-20251208.sh

---
+++ kolokwium-bash-20251208.sh
@@ -0,0 +1,19 @@
+#!\usr\bin\bash
+
+# 1. Stwórz kod za pomocą, którego z liczb w zakresie 0-150 wyświetlone zostaną liczby podzielne przez 4 i nie podzielne przez 10
+
+# 2. Stwórz kod za pomocą, którego z w zakresie 5 – 115 zostaną wyświetlone liczby nieparzyste większe od 12 i mniejsze od 112
+
+# 3. Stwórz kod za pomocą, którego z liczb w zakresie 0-1000 wyświetlone zostaną liczby podzielne przez 50 i nie podzielne przez 200
+
+# 4. Korzystając z pętli while napisz kod, który wyświetli liczby od 37 do 21 z pominięciem liczb parzystych
+
+# 5. Korzystając z pętli while napisz kod, który wyświetli liczby od 17 do 4 z pominięciem liczb 11,9,3
+
+# 6. Korzystając z pętli while napisz kod, który wyświetli liczby od 19 do 11 z pominięciem liczb od 12 do 17
+
+# 7. Korzystając z pętli oblicz sumę wartości nieparzystych wpisanych w tablicę. 
+
+# 8. Stwórz kod sortujący tablicę ustawiając wartości od najmniejszej do największej co iterację wyświetl aktualny stan tablicy
+
+
